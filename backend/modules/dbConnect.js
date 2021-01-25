decb1dc349b0be32fe804f9322d6bdbc6c7b6eeb
kim yongmin <kimyongmin1@naver.com>
2021-01-25 17:20:53 +0900

    mybatis test

--- backend/modules/dbConnect.js
+++ backend/modules/dbConnect.js
@@ -1,19 +1,29 @@
 const mariadb = require('mariadb');
+const mybatis = require('mybatis-mapper');
+const path = require('path');
+const mapperPath = path.join(__dirname, "../");
 
 const pool = mariadb.createPool({
-    host: '128.1.1.10',
+    host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: '1234',
-    database: 'test'
+    password: '!Yong1121',
+    database: 'ymdb'
 });
+
+mybatis.createMapper([path.join(mapperPath, "/mapper/test.xml")]);
+console.log("@@@@@@@@@@@", path.basename(mapperPath));
+
+var query = mybatis.getStatement('tbBbs', 'selectTbBbs', { "name": "맘마" }, { language: 'sql', indent: '  ' });
+console.log(query);
 
 async function getList() {
     let conn, rows;
     try {
         conn = await pool.getConnection();
         conn.query('use test');
-        rows = await conn.query('select * from TB_BBS');
+        rows = await conn.query(query);
+        console.log(query);
     } catch (err) {
         console.log(err);
     } finally {
